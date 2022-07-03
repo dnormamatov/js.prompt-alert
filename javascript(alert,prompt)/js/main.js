7430eaeba00ca68532bd0dd55def47d906133732
dnormamatov <106436702+dnormamatov@users.noreply.github.com>
2022-07-03 06:55:56 +0300

    Add files via upload

---
+++ javascript(alert,prompt)/js/main.js
@@ -0,0 +1,12 @@
+alert("Are you ready?")
+
+var promptInput = prompt("what is your name?");
+var promptSurname = prompt("what is your surname?");
+var promptLogin = prompt("what is your login?");
+var promptPassword = prompt("what is your Password?");
+var promptEmail = prompt("what is your private e-mail");
+var promptNumber = prompt("what is your phone number?");
+
+
+
+console.log('Name: ${promptInput}, \nSurname:${promptSurname}, \nLogin:${promptLogin}, \nPassword:${promptPassword}, \nEmail:${promptEmail}, \nNumber:${prompNumber},')
